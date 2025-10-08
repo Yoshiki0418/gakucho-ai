5c2b2797b9d1d7ac33c3fcd6b613676ef2e7cf1c
LMMadmin <lmm2dreamers@gmail.com>
2025-10-08 22:40:49 +0900

    fix: styled-components の型定義に ESLint 無効化コメントを追加

--- frontend/styles/styled-components.d.ts
+++ frontend/styles/styled-components.d.ts
@@ -6,5 +6,6 @@ type ThemeType = typeof theme;
 
 // styled-components の DefaultTheme にマージ
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
 }
